87f6835f451f0c41699e3a4254f07852bf3b2f34
Tasha Harrison <pete@spirelightmedia.com>
2014-04-02 12:20:16 +0200

    fixing susy by changing to susyone plus added template for jplayer view on frontpage

--- themes/mrregobeats/template.php
+++ themes/mrregobeats/template.php
@@ -5,3 +5,31 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * mrregobeats theme.
  */
+
+
+/**
+ * Implementation of hook_theme().
+ */
+ 
+include_once(drupal_get_path('module', 'jplayer') .'/includes/jplayer.theme.inc');
+
+function jplayer_mrregobeats() {
+  return array(
+    'jplayer_formatter_jplayer_single' => array( // <-- the array key, translated to Drupal tpl is jplayer-formatter-jplayer-single.tpl.php
+      'arguments' => array('element' => NULL),
+      'template' => 'theme/jplayer',
+      'file' => 'includes/jplayer.theme.inc',
+    ),
+    'jplayer_formatter_jplayer_playlist' => array( // <-- ditto for playlist
+      'arguments' => array('element' => NULL),
+      'template' => 'theme/jplayer',
+      'file' => 'includes/jplayer.theme.inc',
+    ),
+    'jplayer_view_playlist' => array( // <-- ditto for views player
+      'arguments' => array('view' => NULL, 'items' => NULL),
+      'template' => 'mrregobeats/templates',
+      'file' => 'includes/jplayer.theme.inc',
+    ),
+  );
+}
+?>
