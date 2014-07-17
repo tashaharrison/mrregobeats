daab2f2e032b780c4190fdc57fd2fa0b5680d37f
Tasha Harrison <pete@spirelightmedia.com>
2014-07-17 18:48:24 +0200

    themed cart confirmation and added js to make it work properly. themed other messages

--- themes/mrregobeats/js/script.js
+++ themes/mrregobeats/js/script.js
@@ -13,3 +13,22 @@ Drupal.behaviors.stickyMenu = {
 };
 
 })(jQuery);
+
+(function ($) {
+  Drupal.behaviors.commerce_add_to_cart_confirmation_overlay = {
+    attach:function (context, settings) {
+      if ($('.messages--commerce-add-to-cart-confirmation').length > 0) {
+        // Add the background overlay.
+        $('body').append("<div class=\"commerce_add_to_cart_confirmation_overlay\"></div>");
+
+        // Enable the close link.
+        $('.commerce-add-to-cart-confirmation-close').live('click touchend', function(e) {
+          e.preventDefault();
+          $('.messages--commerce-add-to-cart-confirmation').remove();
+          $('.commerce_add_to_cart_confirmation_overlay').remove();
+        });
+      }
+    }
+  }
+})(jQuery);
+;
