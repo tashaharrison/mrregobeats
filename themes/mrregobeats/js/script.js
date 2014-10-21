0e5f5b6257ab5ea87276a551cedacd76f18d135d
Tasha Harrison <pete@spirelightmedia.com>
2014-10-21 12:13:43 +0200

    responsive menu

--- themes/mrregobeats/js/script.js
+++ themes/mrregobeats/js/script.js
@@ -53,7 +53,48 @@ Drupal.behaviors.buybuttonHome = {
 		
 	}
 };
-  
+
+/**
+* Responsive menu
+*/	  
+Drupal.behaviors.menuRespond = {
+	attach: function() {
+	
+		var widthSmallMobile = window.matchMedia('only screen and (max-width: 44em)');
+		
+		if (widthSmallMobile.matches == true) {
+			$('.menu li').hide();
+			/**
+ * Responsive menu
+ */	  
+Drupal.behaviors.menuRespond = {
+	attach: function() {
+	
+		var widthSmallMobile = window.matchMedia('only screen and (max-width: 44em)');
+		
+		if (widthSmallMobile.matches == true) {
+			$('.menu li').hide();
+			$('#block-system-main-menu').prepend('<div class="menu-icon close"><div class="menu-button">Menu</div></div>');
+			
+			$('.menu-icon').click(function() {
+				$('.menu li').slideToggle('slow');
+				$('.menu-icon').toggleClass('open');	
+				$('.menu-icon').toggleClass('close');
+			});
+		}
+	
+	}
+};
+			
+			$('.menu-icon').click(function() {
+				$('.menu li').slideToggle('slow');
+				$('.menu-icon').toggleClass('open');	
+				$('.menu-icon').toggleClass('close');
+			});
+		}
+	
+	}
+};
   
 })(jQuery);
 ;
