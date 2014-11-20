f12aa2a215d1ad46771eccc44b035760bfdaf75b
Tasha Harrison <pete@spirelightmedia.com>
2014-11-20 14:43:30 +0100

    mended responsive menus and added tweets to homepage

--- themes/mrregobeats/js/script.js
+++ themes/mrregobeats/js/script.js
@@ -60,20 +60,21 @@ Drupal.behaviors.buybuttonHome = {
  */	  
 Drupal.behaviors.menuRespond = {
 	attach: function() {
-		
-		var widthSmallMobile = window.matchMedia('only screen and (max-width: 44em)');
-			
-		if (widthSmallMobile.matches == true) {
-			$('.menu li').hide();
-			
-			$('#block-system-main-menu').prepend('<div class="menu-icon close"><div class="menu-button">Menu</div></div>');
+		$('#block-system-main-menu').once('menuRespond', function () {
+			var widthSmallMobile = window.matchMedia('only screen and (max-width: 44em)');
+				
+			if (widthSmallMobile.matches == true) {
+				$('.menu li').hide();
+				
+				$('#block-system-main-menu').prepend('<div class="menu-icon close"><div class="menu-button">Menu</div></div>');
 
-			$('.menu-icon').click(function() {
-				$('.menu li').slideToggle('slow');
-				$('.menu-icon').toggleClass('open');	
-				$('.menu-icon').toggleClass('close');
-			});
-		}
+				$('.menu-icon').click(function() {
+					$('.menu li').slideToggle('slow');
+					$('.menu-icon').toggleClass('open');	
+					$('.menu-icon').toggleClass('close');
+				});
+			}
+		});
 	}
 };
 
